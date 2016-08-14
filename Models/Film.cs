7509fd0b7a09ee83a1715403186ab7014fb83458
Matt McCabe <mattmadethis2@gmail.com>
2016-08-14 22:14:33 +0100

    Added Models

---
+++ Models/Film.cs
@@ -0,0 +1,11 @@
+namespace DapperSW.Models
+{
+    public class Film
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public int ReleaseYear { get; set; }
+        public string Rating { get; set; }
+    }
+}
