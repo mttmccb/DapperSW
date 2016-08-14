586d379317e6e1cd36db0f7ca7ccc36ce5be9d4a
Matt McCabe <mattmadethis2@gmail.com>
2016-08-14 22:14:48 +0100

    Added ViewModels

---
+++ ViewModels/FilmDetails.cs
@@ -0,0 +1,16 @@
+using System.Collections.Generic;
+using DapperSW.Models;
+
+namespace DapperSW.FilmDetails
+{
+    public class FilmDetails
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public int ReleaseYear { get; set; }
+        public string Rating { get; set; }
+
+        public List<Actor> Actors { get; set;}
+        public List<Category> Categories { get; set;}
+    }
+}
---
+++ ViewModels/FilmList.cs
@@ -0,0 +1,10 @@
+using System.Collections.Generic;
+using DapperSW.Models;
+
+namespace DapperSW.FilmDetails
+{
+    public class FilmList
+    {
+        public List<Film> Films { get; set;}
+    }
+}
